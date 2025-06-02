5d258d7238be77b8cb4c2a5eec98c89f400da0df
lambretinha <lambletdasilvaj01@gmail.com>
2025-06-02 10:43:52 -0300

    hardware map feito e inicio do teleop

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opModes/Teleop.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opModes/Teleop.java
@@ -1,4 +1,23 @@
 package org.firstinspires.ftc.teamcode.opModes;
 
-public class Teleop {
+import com.acmerobotics.dashboard.config.Config;
+import com.arcrobotics.ftclib.gamepad.GamepadEx;
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+
+@TeleOp(name="Teleop mimindo")
+@Config
+public class Teleop extends OpMode {
+
+    GamepadEx gamepadEx1,gamepadEx2;
+
+    @Override
+    public void init() {
+
+    }
+
+    @Override
+    public void loop() {
+
+    }
 }
